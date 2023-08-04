30b8913743d50c765c04866a719e019daf22b35d
JzomitiZumatti <evgeniyshinkar@gmail.com>
2023-08-04 20:42:30 +0300

    third message

--- Src/ProgramLesson4.java
+++ Src/ProgramLesson4.java
@@ -2,5 +2,6 @@ public class ProgramLesson4 {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello Git from Evhenii");
+        System.out.println("Hello Git from our group");
     }
 }
